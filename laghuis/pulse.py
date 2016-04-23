acc729dd2b57bc6b8032f2ab4b42985e18d4057d
Simon Cross <hodgestar@gmail.com>
2016-04-23 02:29:02 +0200

    Better names for pulse src and sink finders.

--- laghuis/pulse.py
+++ laghuis/pulse.py
@@ -11,10 +11,11 @@ ELEM_RES = {
 USB_SND_DESC = "Description: CM108 Audio"
 
 
-def find_snd_elements(elem_type, elem_desc=USB_SND_DESC):
+def _find_pulse_elements(elem_type, elem_desc=USB_SND_DESC):
     """ Find sound sources or sinks that match a description.
 
     :param elem_type: Either "sources" or "sinks".
+    :param elem_desc: Element description to filter on.
     """
     elem_re = ELEM_RES[elem_type]
     elems = []
@@ -23,18 +24,18 @@ def find_snd_elements(elem_type, elem_desc=USB_SND_DESC):
     for line in output.splitlines():
         match = elem_re.match(line)
         if match is not None:
-            current_device = match.group('device')
+            current_device = int(match.group('device'))
             continue
         if elem_desc in line and current_device is not None:
             elems.append(current_device)
     return elems
 
 
-def find_snd_srcs(elem_desc=USB_SND_DESC):
+def find_pulse_srcs(elem_desc=USB_SND_DESC):
     """ Find sound sources. """
-    return find_snd_elements("sources", elem_desc=elem_desc)
+    return _find_pulse_elements("sources", elem_desc=elem_desc)
 
 
-def find_snd_sinks(elem_desc=USB_SND_DESC):
+def find_pulse_sinks(elem_desc=USB_SND_DESC):
     """ Find sound sinks. """
-    return find_snd_elements("sinks", elem_desc=elem_desc)
+    return _find_pulse_elements("sinks", elem_desc=elem_desc)
