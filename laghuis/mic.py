0e0a94d483a3560b83433fd7457da8cad5ffc9d1
Simon Cross <hodgestar@gmail.com>
2016-04-23 02:30:02 +0200

    Clean up mic.

--- laghuis/mic.py
+++ laghuis/mic.py
@@ -5,11 +5,10 @@ from .box import Box
 
 
 class Mic(Box):
-    def __init__(self, pipeline, name, mic):
+    def __init__(self, pipeline, name, device):
         super(Mic, self).__init__(name, pipeline)
         self.add_sequence([
-            'pulsesrc device=%d name=%s_pulsesrc_%d ' % (
-                mic, name, mic),
-            'equalizer-10bands ',
-            'tee '
+            'pulsesrc name=src device=%d' % (device,),
+            'equalizer-10bands',
+            'tee',
         ])
