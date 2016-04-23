44c1f96e6b3451b6e6159ab136f69c581b3616e1
Simon Cross <hodgestar@gmail.com>
2016-04-23 02:31:13 +0200

    Clean up voices.

--- laghuis/voices.py
+++ laghuis/voices.py
@@ -8,10 +8,10 @@ class FileVoice(Box):
     def __init__(self, pipeline, name, location):
         super(FileVoice, self).__init__(name, pipeline)
         self.add_sequence([
-            'filesrc location=%s ' % location,
-            'decodebin ',
-            'audioconvert ',
-            'audioresample '
+            'filesrc location=%s' % location,
+            'decodebin',
+            'audioconvert',
+            'audioresample',
         ])
         self.elements['decodebin'].connect(
             "pad-added", self.decodebin_pad_added)
@@ -25,6 +25,6 @@ class EchoVoice(Box):
     def __init__(self, pipeline, name, delay, max_delay, feedback, intensity):
         super(EchoVoice, self).__init__(name, pipeline)
         self.add_sequence([
-            'audioecho delay=%d max-delay=%d feedback=%g intensity=%g ' %
-            (delay, max_delay, feedback, intensity)
+            'audioecho delay=%d max-delay=%d feedback=%g intensity=%g' % (
+                delay, max_delay, feedback, intensity),
         ])
