0f1a8cad07b34731129a323d7d4e20b259b21a8a
Simon Cross <hodgestar@gmail.com>
2016-04-25 02:50:02 +0200

    More things for launching as a script.

--- utils/rc.lag.sh
+++ utils/rc.lag.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -e
 
-/home/pi/laghuis/utils/lag-jack.sh >> /home/pi/rc-lag.log
+echo "Setting up dbus environment variables ..."
+
+DBUS_ENV=`dbus-launch`
+for VAR in $DBUS_ENV
+do
+  export $VAR
+done
+
+export HOME="/home/pi"
+
+exec /home/pi/laghuis/utils/lag-jack.sh service 2>&1 >/home/pi/rc-lag.log
